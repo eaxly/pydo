ac1feb941d9383e73dcafb70c532ad098f97170c
Samuel K <mrsosaki@posteo.net>
2021-06-20 12:05:29 +0100

    Make life a little easier for devs README: Add more info for devs etc REQUIREMENTS: Add requirements.txt file MAIN: Add Better comments

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # PYdo
 
 Small to do list application for your Terminal (Work In Progress)
+
+# Quick start guide
+Aha, I see you wan't to use `pydo` please note that it is still **VERY** unstable and unusable. It is not meant for production use.  
+
+# Contribution guide
+So you have decided to waste your free time contributing to this project. **AWESOME!**  
+Before you get lost and rip your hair out, here are some tips on how to use this thing.  
+
+- Use a Code Editor (pretty obvious)
+- Install the dependencies
+  - Install the requirements with either pipenv (by running `pipenv install`)
+  - or you can use standard pip in a virtualenv (run `pip3 -r requirements.txt` in the root of this project.)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 
 import os
 from pathlib import Path
-from rich import style
 from rich.console import Console 
 from rich.prompt import Prompt
 import json
@@ -11,14 +10,16 @@ import json
 console = Console()
 err_console = Console(stderr=True)
 
+# Opening the json file where the tasks are stored and the closing it again (with auto closes files)
 # PATH_TASK_FILE = os.path.join(Path.home(), ".pydos.json")
 PATH_TASK_FILE = os.path.join("pydos.json")
 with open(PATH_TASK_FILE, 'r') as tasks:
     tasks = json.load(tasks)
 
+# Setting the todo and done task lists
 tasks_todo = tasks["tasks_todo"]
 tasks_done = tasks["tasks_done"]
-
+# For some nice console printint later down the line
 def noice(msg_string: str, msg_type: str) -> str:
   """
   A function to print nice informational (noice) fancy text to the terminal.
@@ -48,25 +49,34 @@ def noice(msg_string: str, msg_type: str) -> str:
 def print_tasks(*task_lists, task_list, task_list_done):
 
   for task_list in task_lists:
-    # Looping through the created tuple to access the dictionaries.
+    # ↑↑↑
+    # This method accepts unlimited parameters
+    # the Parameters are later stored in a tuple
+    # So we need to loop through the touple to access the different task_lists passed.
+    # Looping through the created tuple to access the dictionaries ↑↑↑
     
     for id, task in task_list.items():
+      # Looping through the id and task of task_list.items(), more about this method can be found here https://docs.python.org/3/tutorial/datastructures.html?highlight=dictionaries#looping-techniques
+      # Setting the emoji for undone tasks
       emoji = ":black_square_button:"
       if task in task_list_done.items():
+        # setting the emojis for done tasks
         emoji = ":white_check_mark:"
+      # Printing the tasks in the following format: [emoji] [id]. [task]
       console.print(f"{emoji} [bold blue]{id}.[/] {task}")
 
 def add_task(task: str, task_list: dict, tasks_to_write: dict):
   
+  # making an empty list, keys
   keys = []
   for key in tasks_todo.keys():
-    print(type(key))
+    # sorting the tasks_todo keys so that we can the biggest key and add +1 to it.
     int_key = int(key)
     keys.append(int_key)
     keys = sorted(keys)
   
   task_list[keys[-1] + 1] = task
-  
+  # Opening the file to write the task file.
   with open(PATH_TASK_FILE, "w") as tasks:
     json.dump(tasks_to_write, tasks)
 
